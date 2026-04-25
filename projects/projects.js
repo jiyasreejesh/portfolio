cc2686e1f13328b236fee3ebedcbf690237e73c9
jiyasreejesh <jsreejesh@ucsd.edu>
2026-04-24 21:32:34 -0700

    idek

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+console.log("PROJECTS:", projects); // <-- add this
+
 const container = document.querySelector('.projects');
 
 renderProjects(projects, container, 'h2');
