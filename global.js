e167012054f1470f529f9496fb27c8234366c244
jiyasreejesh <jsreejesh@ucsd.edu>
2026-04-24 20:43:43 -0700

    attempted fix

--- global.js
+++ global.js
@@ -5,55 +5,55 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// ==========================
-// STEP 2: AUTO CURRENT PAGE
-// ==========================
-let navLinks = $$("nav a");
+document.addEventListener("DOMContentLoaded", () => {
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+  // ==========================
+  // STEP 2: AUTO CURRENT PAGE
+  // ==========================
+  let navLinks = $$("nav a");
 
-currentLink?.classList.add("current");
-
-// ==========================
-// STEP 4: DARK MODE
-// ==========================
-
-// add dropdown
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `
-  <label class="color-scheme">
-    Theme:
-    <select>
-      <option value="light dark">Auto</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select>
-  </label>
-`
-);
-
-let select = document.querySelector("select");
-
-// load saved preference
-if ("colorScheme" in localStorage) {
-  document.documentElement.style.setProperty(
-    "color-scheme",
-    localStorage.colorScheme
+  let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
   );
-  select.value = localStorage.colorScheme;
-}
 
-// listen for changes
-select.addEventListener("input", (event) => {
-  let value = event.target.value;
+  currentLink?.classList.add("current");
 
-  document.documentElement.style.setProperty("color-scheme", value);
+  // ==========================
+  // DARK MODE
+  // ==========================
 
-  localStorage.colorScheme = value;
-}
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Auto</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+  );
+
+  let select = document.querySelector(".color-scheme select");
+
+  if ("colorScheme" in localStorage) {
+    document.documentElement.style.setProperty(
+      "color-scheme",
+      localStorage.colorScheme
+    );
+    select.value = localStorage.colorScheme;
+  }
+
+  select?.addEventListener("input", (event) => {
+    let value = event.target.value;
+
+    document.documentElement.style.setProperty("color-scheme", value);
+    localStorage.colorScheme = value;
+  });
+
+});
 // ==========================
 // LAB 4: FETCH JSON
 // ==========================
