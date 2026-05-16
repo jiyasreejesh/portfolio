a23e7b1737e9e0cb4d7f8072dac990dc66c7613f
jiyasreejesh <jsreejesh@ucsd.edu>
2026-05-15 20:41:20 -0700

    lab 7

--- global.js
+++ global.js
@@ -86,10 +86,18 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     const article = document.createElement('article');
 
     article.innerHTML = `
-      <${headingLevel}>${project.title}</${headingLevel}>
-      <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
-    `;
+  <${headingLevel}>${project.title}</${headingLevel}>
+  <img src="${project.image}" alt="${project.title}">
+  <div>
+    <p>${project.description}</p>
+    <p class="project-year">${project.year}</p>
+    ${
+      project.url
+        ? `<a href="${project.url}" target="_blank">View project</a>`
+        : ''
+    }
+  </div>
+`;
 
     containerElement.appendChild(article);
   }
